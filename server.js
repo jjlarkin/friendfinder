942473e6a121872c0fda806a45c984f2d72af468
jjlarkin <jjlarkin85@gmail.com>
2017-05-10 16:59:15 -0400

    app.gets working

--- server.js
+++ server.js
@@ -1,3 +1,31 @@
 /**
- *
+ *  Friend Finder
  */
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+
+let app = express();
+
+let PORT = process.env.PORT || 8080;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
